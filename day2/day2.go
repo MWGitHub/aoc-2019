a9c3c4eb263602ec56d0565f91fa573c6e6b316a
mwwmail@gmail.com <mwwmail@gmail.com>
2019-12-08 22:20:40 +0000

    feat: add day 2 part 2 solution

--- day2/day2.go
+++ day2/day2.go
@@ -14,31 +14,67 @@ const (
 	halt     = 99
 )
 
-// IntCoder calculates the IntCode for codes.
-// Out of bounds are not handled.
-func IntCoder(codes []int) []int {
+const (
+	min = 0
+	max = 99
+)
+
+// IntCoder calculates the IntCode for codes and returns a new array of codes.
+func IntCoder(codes []int, noun int, verb int) ([]int, error) {
 	intCodes := make([]int, len(codes))
 	copy(intCodes, codes)
+	intCodes[1] = noun
+	intCodes[2] = verb
 
 	for i := 0; i < len(intCodes); i += 4 {
 		op := intCodes[i]
 		if op == halt {
 			break
 		}
-
-		x := intCodes[intCodes[i+1]]
-		y := intCodes[intCodes[i+2]]
+		// range check
+		length := len(intCodes)
+		if length-i < 4 {
+			return nil, errors.New("indices out of bounds")
+		}
+		pos1 := intCodes[i+1]
+		pos2 := intCodes[i+2]
 		outPos := intCodes[i+3]
+		if pos1 >= length || pos2 >= length || outPos >= length {
+			return nil, errors.New("positions out of bounds")
+		}
+
+		x := intCodes[pos1]
+		y := intCodes[pos2]
 		if op == sum {
 			intCodes[outPos] = x + y
 		} else if op == multiply {
 			intCodes[outPos] = x * y
 		} else {
-			panic(errors.New("invalid opcode"))
+			return nil, errors.New("invalid opcode")
 		}
 	}
 
-	return intCodes
+	return intCodes, nil
+}
+
+// FindNounAndVerb finds the noun and verb that will result in the desired output.
+// Output returns -1 and the error when an error occurs.
+// Brute force method with O(n)
+// More specific: O(c*m*n) with m maximum as max(100,n)^2 and c for array intialization and copying
+func FindNounAndVerb(codes []int, desiredOutput int) (int, error) {
+	for noun := min; noun <= max; noun++ {
+		for verb := min; verb <= max; verb++ {
+			result, err := IntCoder(codes, noun, verb)
+			if err != nil {
+				continue
+			}
+
+			if result[0] == desiredOutput {
+				return 100*noun + verb, nil
+			}
+		}
+	}
+	return -1, errors.New("no noun and verb combination possible")
 }
 
 func main() {
@@ -53,9 +89,14 @@ func main() {
 		}
 		codes[i] = code
 	}
-	// start with 1202 program alarm state
-	codes[1] = 12
-	codes[2] = 2
-
-	fmt.Println(IntCoder(codes)[0])
+	// starting with 1202 program alarm state
+	result, err := IntCoder(codes, 12, 2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result[0])
+	// finding a noun and verb with an output of 19690720
+	const desiredOutput = 19690720
+	nounVerb, err := FindNounAndVerb(codes, desiredOutput)
+	fmt.Println(nounVerb)
 }
--- day2/day2_test.go
+++ day2/day2_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestIntCoder(t *testing.T) {
 	t.Run("addition", func(t *testing.T) {
-		got := IntCoder([]int{1, 0, 0, 0, 99})
+		got, _ := IntCoder([]int{1, 0, 0, 0, 99}, 0, 0)
 		want := []int{2, 0, 0, 0, 99}
 
 		if !reflect.DeepEqual(got, want) {
@@ -16,7 +16,7 @@ func TestIntCoder(t *testing.T) {
 	})
 
 	t.Run("multiplication", func(t *testing.T) {
-		got := IntCoder([]int{2, 3, 0, 3, 99})
+		got, _ := IntCoder([]int{2, 3, 0, 3, 99}, 3, 0)
 		want := []int{2, 3, 0, 6, 99}
 
 		if !reflect.DeepEqual(got, want) {
@@ -25,7 +25,7 @@ func TestIntCoder(t *testing.T) {
 	})
 
 	t.Run("halting", func(t *testing.T) {
-		got := IntCoder([]int{2, 4, 4, 5, 99, 0})
+		got, _ := IntCoder([]int{2, 4, 4, 5, 99, 0}, 4, 4)
 		want := []int{2, 4, 4, 5, 99, 9801}
 
 		if !reflect.DeepEqual(got, want) {
@@ -34,11 +34,30 @@ func TestIntCoder(t *testing.T) {
 	})
 
 	t.Run("halt replace", func(t *testing.T) {
-		got := IntCoder([]int{1, 1, 1, 4, 99, 5, 6, 0, 99})
+		got, _ := IntCoder([]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, 1, 1)
 		want := []int{30, 1, 1, 4, 2, 5, 6, 0, 99}
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("with bad opcode", func(t *testing.T) {
+		_, got := IntCoder([]int{5, 0, 0, 0, 99}, 0, 0)
+
+		if got == nil {
+			t.Errorf("got %s expected error", got)
+		}
+	})
+}
+
+func TestFindNounAndVerb(t *testing.T) {
+	t.Run("with simple desired output", func(t *testing.T) {
+		got, _ := FindNounAndVerb([]int{1, 0, 0, 0, 99}, 100)
+		want := 4
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
 		}
 	})
 }
